35036fcbc0f0bd18653327fb0f744e3a20a55c8f
bskcontactwise <satheeshkumar@contactwise.io>
2021-03-31 21:21:57 +0530

    nabar changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,8 @@ export default function Home() {
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="">About</Nav.Link>
+      <Nav.Link href="">Products</Nav.Link>
       <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
