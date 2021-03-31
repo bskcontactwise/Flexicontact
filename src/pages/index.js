35ae07c4d2d9e64927009a0aa2d94486863120ab
bskcontactwise <satheeshkumar@contactwise.io>
2021-03-31 21:29:14 +0530

    navbar link remove

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,26 +7,15 @@ import Navbar from 'react-bootstrap/Navbar'
 export default function Home() {
   return <>
   <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand href="#home">FlexiContact</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#">About</Nav.Link>
-      <Nav.Link href="#">Products</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#">Separated link</NavDropdown.Item>
-      </NavDropdown>
+      <Nav.Link href="#about">About</Nav.Link>
+      <Nav.Link href="#Products">Products</Nav.Link>
+     
     </Nav>
-    <Nav>
-      <Nav.Link href="#">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#">
-        Dank memes
-      </Nav.Link>
-    </Nav>
+    
   </Navbar.Collapse>
 </Navbar>
   
