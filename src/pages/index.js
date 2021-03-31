aee01aefc5dfd163d3a6468517ad915bd2971452
bskcontactwise <satheeshkumar@contactwise.io>
2021-03-31 20:44:22 +0530

    change text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
 
 export default function Home() {
-  return <div>FlexiContact</div>
+  return <div><h1>Flexicontact</h1>
+  <h2>Hosted Voice for your Business</h2>
+  <p>A cost-effective cloud based phone system, giving your business greater flexibility and control</p>
+
+
+  </div>
 }
